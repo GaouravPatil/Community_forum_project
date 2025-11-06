a2ad6c5e72063aa03c6ae0aec2bb3cb376782f89
rameshsadegaonkar14-web <rameshsadegaonkar14@gmail.com>
2025-11-06 12:09:24 +0530

    fixed the category issue

--- chat/urls.py
+++ chat/urls.py
@@ -13,5 +13,10 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('category/<int:pk>/', views.category_detail, name='category_detail'),
 
+    
+    
 ]
+
+
